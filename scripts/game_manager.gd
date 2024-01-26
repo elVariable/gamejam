7df509269a397adc85e09081f6f2c91941613289
Jan <tehroflz@gmail.com>
2024-01-26 21:16:03 +0100

    Add game_manager

---
+++ scripts/game_manager.gd
@@ -0,0 +1,18 @@
+# This script is an autoload, that can be accessed from any other script!
+
+extends Node2D
+
+var score : int = 0
+var death_counter : int = 0
+
+# Adds 1 to score variable
+func add_score(num = 1):
+    score += num
+
+func add_death():
+    death_counter += 1
+    print(death_counter)
+
+## Loads next level
+#func load_next_level(next_scene : PackedScene):
+    #get_tree().change_scene_to_packed(next_scene)
