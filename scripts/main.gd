8815fe14e81b3ef06d97e06f7e9b0b7657161544
Oliver Plate <68760168+elVariable@users.noreply.github.com>
2024-01-26 21:49:32 +0100

    enemy spawns

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var enemy_scene = load("scences/enemy.tscn")
-var enemy_spawn_timer = 3 # seconds
+var enemy_spawn_timer = 2 # seconds
 var enemy_spawn_amount = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -17,9 +17,8 @@ func enemy_spawner(delta):
     if enemy_spawn_timer > 0:
         enemy_spawn_timer -= delta
     else:
-        print("now")
         for i in range(enemy_spawn_amount):
             var enemy_instance = enemy_scene.instantiate()
-            enemy_instance.position = Vector2(randi() % 1000, randi() % 1000)
             add_child(enemy_instance)
+            enemy_instance.position = Vector2(randi() % 1000, randi() % 1000)
         enemy_spawn_timer = 5
