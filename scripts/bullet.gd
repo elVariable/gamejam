39703199a555208c6d26a910510ece911f4043a6
Jan <tehroflz@gmail.com>
2024-01-26 23:41:57 +0100

    Add bullet

---
+++ scripts/bullet.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export
+var speed = 70
+@export
+var direction = Vector2(1, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+    var motion = direction * speed * delta
+
+    move_and_collide(motion)
+
+
