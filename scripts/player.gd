71107ee5b4688419d1dc5f156206970317351347
Jan <tehroflz@gmail.com>
2024-01-26 19:19:12 +0100

    Add fire buttons

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,11 +9,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    movement(delta)
+    movement()
+    fire()
 
-func movement(delta):
+func movement():
     var move_dir = Input.get_vector("Left", "Right", "Up", "Down")
     velocity = move_dir * VELOCITY
 
-    print(position)
     move_and_slide()
+
+func fire():
+    if Input.is_action_just_pressed("Fire0"):
+        print("Fire0")
+        modulate = Color(255, 0, 0)
+    elif Input.is_action_just_pressed("Fire1"):
+        print("Fire1")
+        modulate = Color(0, 255, 0)
+    elif Input.is_action_just_pressed("Fire2"):
+        print("Fire2")
+        modulate = Color(0, 0, 255)
+    elif Input.is_action_just_pressed("Fire3"):
+        print("Fire3")
+        modulate = Color(120, 120, 0)
+    elif Input.is_action_just_released("Fire0") or Input.is_action_just_released("Fire1") or Input.is_action_just_released("Fire2") or Input.is_action_just_released("Fire3"):
+        modulate = Color(255, 255, 255)
