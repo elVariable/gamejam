34e6bff4bb343ed80cd5aa830ede32ea6e3ef1e0
Jan <tehroflz@gmail.com>
2024-01-27 21:05:29 +0100

    Change default weapon

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,7 +18,7 @@ var weapons: Dictionary = {
 # Called when the node enters the scene tree for the first time.
 func _ready():
     position = get_viewport_rect().size * 0.5
-    switch_weapon("hi_weapon")
+    switch_weapon("he_weapon")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
