c1170261c9fb9a48f6382f360422c26229ef3714
Jan <tehroflz@gmail.com>
2024-01-27 21:03:19 +0100

    Add bullet animations

--- scripts/bullets/bullet.gd
+++ scripts/bullets/bullet.gd
@@ -11,7 +11,7 @@ var audio_queue = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $AnimationPlayer.play("shot")
+    $AnimationPlayer.play("flying")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
