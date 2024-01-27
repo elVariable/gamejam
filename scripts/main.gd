3b0549e3546b594d209c6f8534e110f3d6faa66e
Jan <tehroflz@gmail.com>
2024-01-27 18:46:13 +0100

    Add spawn timer

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,7 +7,7 @@ var enemies: Array = [
 ]
 
 var enemy_spawn_timer = 3 # seconds
-var last_enemy_spawn_time = 0
+
 var current_start_difficulty = 1
 var current_difficulty = 1
 
@@ -20,6 +20,7 @@ var current_wave_duration = 0
 var intermediate_scene = load("res://scenes/levels/intermediate.tscn")
 
 var difficulty_timer : Timer = null
+var spawn_timer : Timer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,25 +28,16 @@ func _ready():
 
     difficulty_timer = get_node("DifficutlyTimer")
     difficulty_timer.start(next_enemy_dificulty_increase)
+
+    spawn_timer = get_node("SpawnTimer")
+    spawn_timer.start(enemy_spawn_timer)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    enemy_spawner(delta)
 
     current_wave_duration += delta
     if current_wave_duration > wave_duration:
         end_wave()
-
-func enemy_spawner(delta):
-    if last_enemy_spawn_time < enemy_spawn_timer:
-        last_enemy_spawn_time += delta
-    else:
-        for e in enemies:
-            var enemy_spawn_amount = e.get_enemy_spawn_amount(current_difficulty)
-            for i in range(enemy_spawn_amount):
-                var enemy_instance = e.create_instance()
-                add_child(enemy_instance)
-                enemy_instance.position = e.get_legit_spawn(get_window().size, get_node("%Player").position)
-            last_enemy_spawn_time = 0
 
 func end_wave():
     get_tree().paused = true
@@ -69,8 +61,18 @@ func end_wave():
     for item in get_tree().get_nodes_in_group("Items"):
         item.queue_free()
 
-
 func _on_difficutly_timer_timeout():
     current_difficulty += 1
     next_enemy_dificulty_increase = next_enemy_dificulty_increase * 1.3
+
     difficulty_timer.start(next_enemy_dificulty_increase)
+
+func _on_spawn_timer_timeout():
+    for e in enemies:
+        var enemy_spawn_amount = e.get_enemy_spawn_amount(current_difficulty)
+        for i in range(enemy_spawn_amount):
+            var enemy_instance = e.create_instance()
+            add_child(enemy_instance)
+            enemy_instance.position = e.get_legit_spawn(get_window().size, get_node("%Player").position)
+
+    spawn_timer.start(enemy_spawn_timer)
