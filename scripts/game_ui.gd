05574dc3ece4751f50204a346994580be156c834
Jan <tehroflz@gmail.com>
2024-01-27 01:37:15 +0100

    Add life to ui

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -3,6 +3,7 @@ extends Control
 #@onready var score_texture = %Score/ScoreTexture
 @onready var score_label = %Score/ScoreLabel
 @onready var time_label = %Time/TimeLabel
+@onready var life_label = %Life/LifeLabel
 
 #@onready var death_label = %Death/DeathLabel
 
@@ -14,3 +15,6 @@ func _process(_delta):
     var ingame_time_s = Time.get_ticks_msec() / 1000
     time_label.text = "Time since start %ds" % ingame_time_s
     #death_label.text = "Deaths: %d" % GameManager.death_counter
+
+    var player = get_tree().root.get_node("/root/level/Player")
+    life_label.text = "Life: %d" % player.player_life
