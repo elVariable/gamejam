b64cc2508576d9536a798f90203caa56930ed557
Oliver Plate <68760168+elVariable@users.noreply.github.com>
2024-01-27 04:09:38 +0100

    improvements

--- scripts/intermediate.gd
+++ scripts/intermediate.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var text_updated : bool = false
+var possible_upgrades = ["HA", "HE", "HI", "HO", "HU"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,14 +12,25 @@ func _process(delta):
     if Input.is_action_just_pressed("Exit"):
         get_tree().quit()
 
-    if not text_updated:
-        $Control/Text.clear()
-        $Control/Text.append_text("[b]Wave:[/b] " + str(GameManager.wave) + "\n")
-        $Control/Text.append_text("[b]Score:[/b] " + str(GameManager.score) + "\n")
-        text_updated = true
-
 func _on_continue_pressed():
     hide()
     get_node("/root/level").show()
     get_tree().paused = false
-    text_updated = false
+
+func roll_and_show_upgrades():
+    var op1 = randi() % 5
+    var op2 = randi() % 5
+    var op3 = randi() % 5
+
+    $Control/Option1.text = possible_upgrades[op1]
+    $Control/Option2.text = possible_upgrades[op2]
+    $Control/Option3.text = possible_upgrades[op3]
+
+
+func _on_visibility_changed():
+    if visible:
+        $Control/Text.clear()
+        $Control/Text.append_text("[b]Wave:[/b] " + str(GameManager.wave) + "\n")
+        $Control/Text.append_text("[b]Score:[/b] " + str(GameManager.score) + "\n")
+        
+        roll_and_show_upgrades()
