c4d1b9f443d1c1af40bd818a339075f97d5ef4e8
Jan <tehroflz@gmail.com>
2024-01-27 01:23:11 +0100

    Add dmg to bullets

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -5,6 +5,8 @@ var speed = 700
 @export
 var direction = Vector2(1, 0)
 
+var dmg = 50
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
