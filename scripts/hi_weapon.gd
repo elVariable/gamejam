47de03a6679fac3c5e8c3f7b3ba2431e4401d99d
Jan <tehroflz@gmail.com>
2024-01-27 16:11:43 +0100

    Refactor weapons

--- scripts/hi_weapon.gd
+++ scripts/hi_weapon.gd
@@ -2,11 +2,16 @@ extends Weapon
 
 class_name HiWeapon
 
+func _init():
+    bullet_speed = 1700
+    bullet_dmg = 20
+    bullet_pircing = 1
+    base_spawn_time = 0.15
+    base_spawn_rate = 1
+    bullet_scale = Vector2(0.5, 0.5)
+
+    bullet_scene = load("res://scenes/prefabs/bullets/hi_bullet.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    bullet_speed = 1700
-    bullet_dmg = 10
-    bullet_pircing = 1
-    base_spawn_time = 0.1
-    base_spawn_rate = 1
-    bullet_scale = Vector2(0.3, 0.3)
+    pass
