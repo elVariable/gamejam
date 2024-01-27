8be36b15c2b496ce7448cb2053cc9f41abf97ce0
Jan <tehroflz@gmail.com>
2024-01-27 19:40:23 +0100

    Move fire bullets to new function

--- scripts/weapons/ho_weapon.gd
+++ scripts/weapons/ho_weapon.gd
@@ -5,13 +5,9 @@ class_name HoWeapon
 func _init():
     bullet_speed = 1700
     bullet_dmg = 10
-    bullet_pircing = 1
-    base_spawn_time = 0.1
+    bullet_pircing = 10
+    base_spawn_time = 10
     base_spawn_rate = 1
     bullet_scale = Vector2(3, 3)
 
     bullet_scene = load("res://scenes/prefabs/bullets/ho_bullet.tscn")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass
