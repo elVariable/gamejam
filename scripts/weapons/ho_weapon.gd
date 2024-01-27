a21fd2a3197081e849ca316a39cd202c1f1b2518
Jan <tehroflz@gmail.com>
2024-01-27 19:41:40 +0100

    Tweak HoWeapon

--- scripts/weapons/ho_weapon.gd
+++ scripts/weapons/ho_weapon.gd
@@ -3,10 +3,10 @@ extends Weapon
 class_name HoWeapon
 
 func _init():
-    bullet_speed = 1700
-    bullet_dmg = 10
+    bullet_speed = 100
+    bullet_dmg = 100
     bullet_pircing = 10
-    base_spawn_time = 10
+    base_spawn_time = 2
     base_spawn_rate = 1
     bullet_scale = Vector2(3, 3)
 
