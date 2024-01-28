544a4b7c07ec9a08ed19fd306e9d9239c133a4e4
Oliver Plate <68760168+elVariable@users.noreply.github.com>
2024-01-28 13:29:55 +0100

    audio stuff

--- scripts/bullets/he_bullet.gd
+++ scripts/bullets/he_bullet.gd
@@ -9,4 +9,5 @@ var sounds = [
 
 func _init():
     $DefaultAudio.stream = load(sounds[(int(Time.get_ticks_msec() / 1000) / 2 ) % sounds.size()])
+    $DefaultAudio.pitch_scale = randf_range(0.6, 1.4)
     pass
